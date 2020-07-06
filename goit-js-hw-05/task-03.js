7fdf5cdf75e5c46eff9d692bb039255befe60034
RomanVitovych <romario538@gmail.com>
2020-07-06 18:57:11 +0300

    corect

---
+++ goit-js-hw-05/task-03.js
@@ -0,0 +1,35 @@
+class Storage {
+    constructor(array) {
+        this.items = array;
+    }
+    getItems() {
+        return this.items;
+    }
+    addItem(item) {
+        return this.items.push(items);
+    }
+    removeItem(item) {
+        for (let el = 0; el < this.items.length; el += 1) {
+            console.log(this.items[el]);
+            if (this.items[el] === item) {
+                return this.items.splice(el, 1);
+            }
+        }
+    }
+}
+
+const storage = new Storage([
+    'Нанітоіди',
+    'Пролонгер',
+    'Залізні жупи',
+    'Антигравітатор',
+]);
+
+const items = storage.getItems();
+console.table(items); // [ "Нанітоіди", "Пролонгер", "Залізні жупи", "Антигравітатор" ]
+
+storage.addItem('Дроїд');
+console.table(storage.items); // [ "Нанітоіди", "Пролонгер", "Залізні жупи", "Антигравітатор", "Дроїд" ]
+
+storage.removeItem('Пролонгер');
+console.table(storage.items); // [ "Нанітоіди", "Залізні жупи", "Антигравітатор", "Дроїд" ]
